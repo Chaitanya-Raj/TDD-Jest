866295d0e37df3e83577960db896c5f8b2042058
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-03 16:43:31 +0530

    refactor cipher

--- cipher/cipher.js
+++ cipher/cipher.js
@@ -1,29 +1,37 @@
-function cipher(str, key) {
+function cipher(encodedText, key) {
   let numbers = [];
-  let newSentece = [];
+  let decodedText = [];
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].match(/[a-zA-Z]/)) {
-      numbers.push(str[i].charCodeAt(0));
+  for (let i = 0; i < encodedText.length; i++) {
+    if (encodedText[i].match(/[a-zA-Z]/)) {
+      numbers.push(encodedText[i].charCodeAt(0));
     } else {
-      numbers.push(str[i]);
+      numbers.push(encodedText[i]);
     }
   }
 
   for (let i = 0; i < numbers.length; i++) {
-    if (str[i].match(/[a-zA-Z]/) && numbers[i] >= 65 && numbers[i] <= 90) {
-      newSentece.push(String.fromCharCode(((numbers[i] - 65 + key) % 26) + 65));
+    if (
+      encodedText[i].match(/[a-zA-Z]/) &&
+      numbers[i] >= 65 &&
+      numbers[i] <= 90
+    ) {
+      decodedText.push(
+        String.fromCharCode(((numbers[i] - 65 + key) % 26) + 65)
+      );
     } else if (
-      str[i].match(/[a-zA-Z]/) &&
+      encodedText[i].match(/[a-zA-Z]/) &&
       numbers[i] >= 97 &&
       numbers[i] <= 122
     ) {
-      newSentece.push(String.fromCharCode(((numbers[i] - 97 + key) % 26) + 97));
+      decodedText.push(
+        String.fromCharCode(((numbers[i] - 97 + key) % 26) + 97)
+      );
     } else {
-      newSentece.push(numbers[i]);
+      decodedText.push(numbers[i]);
     }
   }
-  return newSentece.join("");
+  return decodedText.join("");
 }
 
 export default cipher;
